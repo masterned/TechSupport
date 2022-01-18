6e4f0f50d89deadec855b15a91003e4d9a2a1a07
Spencer Dent <sd00070@my.westga.edu>
2022-01-18 01:07:35 -0500

    prune unused imports & document methods

--- TechSupport/View/LoginForm.cs
+++ TechSupport/View/LoginForm.cs
@@ -7,12 +7,24 @@ namespace TechSupport.View
     {
         MainForm mainForm;
 
+        /// <summary>
+        /// Initializes the LoginForm,
+        /// and creates its main form.
+        /// </summary>
         public LoginForm()
         {
             InitializeComponent();
             this.mainForm = new MainForm(this);
         }
 
+        /// <summary>
+        /// Handles the event fired when the login button is clicked.
+        /// Tests the entered username and password,
+        /// and either displays an error message on an unsuccessful attempt
+        /// or hides the login form and shows the main form on a successful one.
+        /// </summary>
+        /// <param name="sender">The "Login" button</param>
+        /// <param name="e">Arguments for the event</param>
         private void LoginButton_Click(object sender, EventArgs e)
         {
             /*
@@ -38,12 +50,24 @@ namespace TechSupport.View
             mainForm.Show();
         }
 
+        /// <summary>
+        /// Handles events fired when text is inputted into one of the TextBoxes.
+        /// Hides the error message if it's visible.
+        /// </summary>
+        /// <param name="sender">The username or password TextBox</param>
+        /// <param name="e">Event arguments</param>
         private void FieldTextBox_TextChanged(object sender, EventArgs e)
         {
             if (sender is TextBox)
                 errorMessageLabel.Visible = false;
         }
 
+        /// <summary>
+        /// Handles the event fired when the form is closing.
+        /// Makes sure that the entire application closes.
+        /// </summary>
+        /// <param name="sender">The Login form</param>
+        /// <param name="e">Event arguments</param>
         private void LoginForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
--- TechSupport/View/MainForm.cs
+++ TechSupport/View/MainForm.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace TechSupport.View
 {
@@ -14,23 +6,42 @@ namespace TechSupport.View
     {
         LoginForm loginForm;
 
+        /// <summary>
+        /// Initializes the component and registers the loginForm.
+        /// </summary>
+        /// <param name="loginForm">Primarily given access in order to Show when needed.</param>
         public MainForm(LoginForm loginForm)
         {
             InitializeComponent();
             this.loginForm = loginForm;
         }
 
+        /// <summary>
+        /// Sets the username in the username Label to the passed value.
+        /// </summary>
+        /// <param name="username">The new string for the username Label text</param>
         public void setUsername(string username)
         {
             usernameLabel.Text = username;
         }
 
+        /// <summary>
+        /// Handles the event fired when the logout LabelLink is clicked.
+        /// </summary>
+        /// <param name="sender">The logout LabelLink</param>
+        /// <param name="e">Event arguments</param>
         private void logoutLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             Hide();
             loginForm.Show();
         }
 
+        /// <summary>
+        /// Handles the event fired when the form is closing.
+        /// Makes sure that the entire application closes.
+        /// </summary>
+        /// <param name="sender">The Main form</param>
+        /// <param name="e">Event arguments</param>
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
