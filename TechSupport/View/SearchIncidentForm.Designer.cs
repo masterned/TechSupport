0e6cae387180ed8e6f3a5c4eccbdf7b3d90a6698
Spencer Dent <sd00070@my.westga.edu>
2022-01-21 16:28:44 -0500

    create "search incident" form view

---
+++ TechSupport/View/SearchIncidentForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace TechSupport.View
+{
+    partial class SearchIncidentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CustomerIDLabel = new System.Windows.Forms.Label();
+            this.CustomerIDTextBox = new System.Windows.Forms.TextBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CustomerIDLabel
+            // 
+            this.CustomerIDLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.CustomerIDLabel.AutoSize = true;
+            this.CustomerIDLabel.Location = new System.Drawing.Point(20, 15);
+            this.CustomerIDLabel.Name = "CustomerIDLabel";
+            this.CustomerIDLabel.Size = new System.Drawing.Size(65, 13);
+            this.CustomerIDLabel.TabIndex = 0;
+            this.CustomerIDLabel.Text = "Customer ID";
+            // 
+            // CustomerIDTextBox
+            // 
+            this.CustomerIDTextBox.Location = new System.Drawing.Point(91, 11);
+            this.CustomerIDTextBox.Name = "CustomerIDTextBox";
+            this.CustomerIDTextBox.Size = new System.Drawing.Size(100, 20);
+            this.CustomerIDTextBox.TabIndex = 1;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(197, 9);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.TabIndex = 2;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // SearchIncidentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(284, 201);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.CustomerIDTextBox);
+            this.Controls.Add(this.CustomerIDLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "SearchIncidentForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Search Incident";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label CustomerIDLabel;
+        private System.Windows.Forms.TextBox CustomerIDTextBox;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
