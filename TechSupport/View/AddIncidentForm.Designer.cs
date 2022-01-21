fe632c1c37e94872ed76120c19386e47b2841913
Spencer Dent <sd00070@my.westga.edu>
2022-01-21 12:10:46 -0500

    create "add incident" form view

---
+++ TechSupport/View/AddIncidentForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace TechSupport.View
+{
+    partial class AddIncidentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.customerIDLabel = new System.Windows.Forms.Label();
+            this.customerIDTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.titleTextBox.Location = new System.Drawing.Point(74, 3);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(204, 20);
+            this.titleTextBox.TabIndex = 0;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(22, 6);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(27, 13);
+            this.titleLabel.TabIndex = 1;
+            this.titleLabel.Text = "Title";
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.descriptionLabel.AutoSize = true;
+            this.descriptionLabel.Location = new System.Drawing.Point(5, 26);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(60, 13);
+            this.descriptionLabel.TabIndex = 2;
+            this.descriptionLabel.Text = "Description";
+            // 
+            // descriptionTextBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.descriptionTextBox, 2);
+            this.descriptionTextBox.Location = new System.Drawing.Point(3, 42);
+            this.descriptionTextBox.Multiline = true;
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.descriptionTextBox.Size = new System.Drawing.Size(275, 60);
+            this.descriptionTextBox.TabIndex = 3;
+            // 
+            // customerIDLabel
+            // 
+            this.customerIDLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.customerIDLabel.AutoSize = true;
+            this.customerIDLabel.Location = new System.Drawing.Point(3, 113);
+            this.customerIDLabel.Name = "customerIDLabel";
+            this.customerIDLabel.Size = new System.Drawing.Size(65, 13);
+            this.customerIDLabel.TabIndex = 4;
+            this.customerIDLabel.Text = "Customer ID";
+            // 
+            // customerIDTextBox
+            // 
+            this.customerIDTextBox.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.customerIDTextBox.Location = new System.Drawing.Point(74, 110);
+            this.customerIDTextBox.Name = "customerIDTextBox";
+            this.customerIDTextBox.Size = new System.Drawing.Size(100, 20);
+            this.customerIDTextBox.TabIndex = 5;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(55, 153);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 6;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(171, 153);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 7;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.titleLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.titleTextBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.customerIDTextBox, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.customerIDLabel, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.descriptionTextBox, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.descriptionLabel, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(280, 135);
+            this.tableLayoutPanel1.TabIndex = 8;
+            // 
+            // AddIncidentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 186);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.addButton);
+            this.Name = "AddIncidentForm";
+            this.Text = "Add Incident";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label descriptionLabel;
+        private System.Windows.Forms.TextBox descriptionTextBox;
+        private System.Windows.Forms.Label customerIDLabel;
+        private System.Windows.Forms.TextBox customerIDTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
