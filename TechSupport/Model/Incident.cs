37865edb860cb21acc451a84135ed35b9cb33725
Spencer Dent <sd00070@my.westga.edu>
2022-01-22 18:14:58 -0500

    create Incident model

---
+++ TechSupport/Model/Incident.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupport.Model
+{
+    class Incident
+    {
+        public string Title { get; }
+
+        public string Description { get; }
+
+        public string CustomerID { get; }
+
+        public Incident(string title, string description, string customerID)
+        {
+            if (string.IsNullOrEmpty(title.Trim()))
+                throw new ArgumentNullException("Title cannot be empty");
+            
+            if (string.IsNullOrEmpty(description.Trim()))
+                throw new ArgumentNullException("Description cannot be empty");
+
+            if (string.IsNullOrEmpty(customerID.Trim()))
+                throw new ArgumentNullException("CustomerID cannot be empty");
+
+            Title = title.Trim();
+            Description = description.Trim();
+            CustomerID = customerID.Trim();
+        }
+    }
+}
