01ce56d6ae3770e09e54baeee0fbe4706d00b78a
Spencer Dent <sd00070@my.westga.edu>
2022-01-31 23:16:33 -0500

    create MainDashboard w/ TabControls

---
+++ TechSupport/View/MainDashboard.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace TechSupport.View
+{
+    partial class MainDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.AddIncidentTabPage = new System.Windows.Forms.TabPage();
+            this.ViewAllIncidentsTabPage = new System.Windows.Forms.TabPage();
+            this.SearchIncidentTabPage = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.AddIncidentTabPage);
+            this.tabControl1.Controls.Add(this.ViewAllIncidentsTabPage);
+            this.tabControl1.Controls.Add(this.SearchIncidentTabPage);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(624, 441);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // AddIncidentTabPage
+            // 
+            this.AddIncidentTabPage.Location = new System.Drawing.Point(4, 22);
+            this.AddIncidentTabPage.Name = "AddIncidentTabPage";
+            this.AddIncidentTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.AddIncidentTabPage.Size = new System.Drawing.Size(616, 415);
+            this.AddIncidentTabPage.TabIndex = 0;
+            this.AddIncidentTabPage.Text = "Add Incident";
+            this.AddIncidentTabPage.UseVisualStyleBackColor = true;
+            // 
+            // ViewAllIncidentsTabPage
+            // 
+            this.ViewAllIncidentsTabPage.Location = new System.Drawing.Point(4, 22);
+            this.ViewAllIncidentsTabPage.Name = "ViewAllIncidentsTabPage";
+            this.ViewAllIncidentsTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.ViewAllIncidentsTabPage.Size = new System.Drawing.Size(616, 415);
+            this.ViewAllIncidentsTabPage.TabIndex = 1;
+            this.ViewAllIncidentsTabPage.Text = "View All Incidents";
+            this.ViewAllIncidentsTabPage.UseVisualStyleBackColor = true;
+            // 
+            // SearchIncidentTabPage
+            // 
+            this.SearchIncidentTabPage.Location = new System.Drawing.Point(4, 22);
+            this.SearchIncidentTabPage.Name = "SearchIncidentTabPage";
+            this.SearchIncidentTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.SearchIncidentTabPage.Size = new System.Drawing.Size(616, 415);
+            this.SearchIncidentTabPage.TabIndex = 2;
+            this.SearchIncidentTabPage.Text = "Search Incident";
+            this.SearchIncidentTabPage.UseVisualStyleBackColor = true;
+            // 
+            // MainDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "MainDashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tech Support";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage AddIncidentTabPage;
+        private System.Windows.Forms.TabPage ViewAllIncidentsTabPage;
+        private System.Windows.Forms.TabPage SearchIncidentTabPage;
+    }
+}
