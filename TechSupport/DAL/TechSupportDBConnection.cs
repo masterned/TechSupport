7600c87bcfb590c05498b8613659fc5de74d127e
Spencer Dent <sd00070@my.westga.edu>
2022-02-08 01:09:31 -0500

    create TechSupport Connection object

---
+++ TechSupport/DAL/TechSupportDBConnection.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+
+namespace TechSupport.DAL
+{
+    /// <summary>
+    /// Sets up a connection to the TechSupport server.
+    /// </summary>
+    public static class TechSupportDBConnection
+    {
+        /// <summary>
+        /// Creates and returns a new Connection to the TechSupport server.
+        /// </summary>
+        /// <returns>A Connection object linked to the TechSupport server</returns>
+        public static SqlConnection GetConnection()
+        {
+            string connectionString =
+                "Data Source=localhost;Initial Catalog=TechSupport;" +
+                "Integrated Security=True";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
