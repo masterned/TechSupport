81dbf154f9b8f0fb41fef2915bc3ad582ce0019d
Spencer Dent <sd00070@my.westga.edu>
2022-03-22 01:40:03 -0400

    add missing xml summaries

--- TechSupport/UserControls/OpenIncidentsReportUserControl.cs
+++ TechSupport/UserControls/OpenIncidentsReportUserControl.cs
@@ -3,8 +3,14 @@ using System.Windows.Forms;
 
 namespace TechSupport.UserControls
 {
+    /// <summary>
+    /// Displays a report containing all open incidents assigned to technicians.
+    /// </summary>
     public partial class OpenIncidentsReportUserControl : UserControl
     {
+        /// <summary>
+        /// Initializes view and sets property values.
+        /// </summary>
         public OpenIncidentsReportUserControl()
         {
             InitializeComponent();
@@ -16,6 +22,9 @@ namespace TechSupport.UserControls
             RefreshReport();
         }
 
+        /// <summary>
+        /// Refreshes the data in the report view.
+        /// </summary>
         public void RefreshReport()
         {
             incidentsTableAdapter.Fill(techSupportDataSet.Incidents);
